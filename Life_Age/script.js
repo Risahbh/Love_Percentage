f26d19a030f025ff3bf401e46302e3cfa521a70a
Risahbh <79095208+Risahbh@users.noreply.github.com>
2022-02-11 03:15:48 -0500

    Add files via upload

---
+++ Life_Age/script.js
@@ -0,0 +1,6 @@
+prompt("what i your Name")
+prompt("what is their Name")
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+alert("Your Love percentage is "+ loveScore)
